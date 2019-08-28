acae38ef54fde50e7c637e58c9d172838b576447
Tania Nelzy <tatou.nel@gmail.com>
2019-08-28 19:38:59 -0400

    adding hello world

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+//
+//  helloworld.cpp
+//  
+//
+//  Created by Tania Nelzy on 8/28/19.
+//  Copyright © 2019 Tania Nelzy. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
